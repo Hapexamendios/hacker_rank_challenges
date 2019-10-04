2db8e3d680342ef8fa818053057053d716233701
Tim Mendt <spearmendt@gmail.com>
2019-10-04 12:31:46 -0600

    Update finding_the_percentage.py
    Cleaned up print statements and added comments

--- Practice/Python/Basic_data_types/finding_the_percentage.py
+++ Practice/Python/Basic_data_types/finding_the_percentage.py
@@ -1,21 +1,35 @@
 numStudents = int(input())
-#print(numStudents)
-increment = 0
+# Obtain the number of students - the number of times to add to the dictionary
+
 studentDict = {}
+# Initialize the dictionary that will hold students and their grades
+
 grades = []
+# Initialize the list that will hold grades
+
 for item in range(0,numStudents):
-    increment += 1
-    #print(increment)
+# Perform the following the number of times there are students (first input)
+
     nameAndGrades = input()
-    #print(nameAndGrades)
+    # Obtain student name and grades
+
     nameAndGrades = nameAndGrades.split()
-    #print(nameAndGrades)
+    # Split name and grades apart into a list
+
     grades = [float(nameAndGrades[1]),float(nameAndGrades[2]),float(nameAndGrades[3])]
+    # Set the grades list equal to the scores, and convert scores to floats from strings
+
     studentDict.update({nameAndGrades[0]:grades})
-#print(studentDict)
+    # Create a dictionary item with the name string as they key and a list containing grades as the associated value
+
 studentToAverage=input()
-#print(studentToAverage)
+# Accept input for the student whos average to find
+
 scoresToAverage=studentDict[studentToAverage]
-#print(scoresToAverage)
+# Obtain the scores of the student in question
+
 average = (scoresToAverage[0]+scoresToAverage[1]+scoresToAverage[2])/3
+# Obtain the average of the three scores of the student in question
+
 print("{:.2f}".format(average))
+# Format the average to be 2 decimal points and output it
